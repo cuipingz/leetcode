fbbf1d310698509028a9a311f2444980b280af22
cuipingz <2310631813@qq.com>
2021-12-25 16:15:57 +0800

    add 1.js“

---
+++ 1.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+       var index = new Array();
+       for(var i = 0; i < nums.length; i++){
+           for(var j = i+1; j < nums.length; j++){
+               if(nums[i] + nums[j] === target){
+                   index[0] = i;
+                   index[1] = j;
+                   break;
+               }
+
+           }
+       }
+       return index;
+};
